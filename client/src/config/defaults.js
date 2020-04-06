567c6b354c5027735daae82ec117e6c4762c5241
Pedro Nunes <preisnunes@gmail.com>
2020-04-06 18:13:48 +0100

    tt-04: adding api to get from bo timespans allowed

--- client/src/config/defaults.js
+++ client/src/config/defaults.js
@@ -1,32 +1,7 @@
 export const searchItems = {
-    limit: 10
+    limit: 3
 }
 
-export const timeWindows = [
-    {
-        id : '-1h',
-        name: 'Last hour'
-    }, 
-    {
-        id: '-1d',
-        name: 'Last day'
-    }, 
-    {
-        id: '-1w',
-        name: 'Last week'
-    }, 
-    {
-        id:'-1m',
-        name: 'Last month'
-    }, 
-    {
-        id: '-1y',
-        name: 'Last year'
-    }, 
-    {
-        id: '-5y',
-        name: 'Last 5 years'
-    }
-];
-
 export const trendsAPI = 'api/v1/trends';
+
+export const timespansAPI = 'api/v1/timespans';
