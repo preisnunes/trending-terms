e80b80b28a054d15cbb152d0a7387156443f3bcf
Pedro Nunes <preisnunes@gmail.com>
2020-04-06 18:09:48 +0100

    tt-04: small refactor and dependencies

--- bo/src/controllers/TermsTrendingController.js
+++ bo/src/controllers/TermsTrendingController.js
@@ -3,17 +3,17 @@ import googleTrendingService from '../services/trending.js';
 export const getInterestOverTime = async function(req, res){
     try{
         const searchItems = JSON.parse(req.query.items);
-        const timeSpan = req.query.span;
-    
+        const timespanCode = !req.query.span ? '-1m' :req.query.span;
+        
         let keywords = [];
         let geo = [];
-    
+        
         searchItems.forEach((item) => {
             keywords.push(item.term);
             geo.push(item.geo);
         });
         
-        const termsInterestOverTime =  await googleTrendingService.fetchInterestOverTime(keywords, geo); 
+        const termsInterestOverTime =  await googleTrendingService.fetchInterestOverTime(keywords, geo, timespanCode); 
         res.send(termsInterestOverTime);
     }
     catch (Exception)
